bbb56128dcc0c56c8495309d615d3fd81455bd96
pdibuono <ptdibuono@quinnipiac.edu>
2015-11-17 18:21:19 +0000

    Add picks and replies to user profiles

--- core/views.py
+++ core/views.py
@@ -133,3 +133,12 @@ class UserDetailView(DetailView):
   slug_field = 'username'
   template_name = 'user/user_detail.html'
   context_object_name = 'user_in_view'
+  
+  def get_context_data(self, **kwargs):
+      context = super(UserDetailView, self).get_context_data(**kwargs)
+      user_in_view = User.objects.get(username=self.kwargs['slug'])
+      picks = Pick.objects.filter(user=user_in_view)
+      context['picks'] = picks
+      replies = Reply.objects.filter(user=user_in_view)
+      context['replies'] = replies
+      return context 
