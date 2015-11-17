c6c2179ba31ba6b94d0c5d31bcc5b118138d5b56
pdibuono <ptdibuono@quinnipiac.edu>
2015-11-17 01:06:42 +0000

    Re-Set up heroku

--- gambling_forum/settings.py
+++ gambling_forum/settings.py
@@ -36,6 +36,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'registration',
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
