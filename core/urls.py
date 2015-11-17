bc92d2cc05959318109127e50a65e00d31ddc6d4
pdibuono <ptdibuono@quinnipiac.edu>
2015-11-17 18:08:38 +0000

    Add user profiles

--- core/urls.py
+++ core/urls.py
@@ -15,4 +15,5 @@ urlpatterns = patterns('',
      url(r'^pick/(?P<pick_pk>\d+)/reply/update/(?P<reply_pk>\d+)/$', login_required(ReplyUpdateView.as_view()), name='reply_update'),
      url(r'^pick/(?P<pick_pk>\d+)/reply/delete/(?P<reply_pk>\d+)/$', login_required(ReplyDeleteView.as_view()), name='reply_delete'),
     url(r'^vote/$', login_required(VoteFormView.as_view()), name='vote'),
+    url(r'^user/(?P<slug>\w+)/$', login_required(UserDetailView.as_view()), name='user_detail'),
 )
